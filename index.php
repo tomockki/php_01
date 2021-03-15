e3412d1a5e81df99f5ca6c62c16c24bd51cb357a
tomockki <80142152+tomockki@users.noreply.github.com>
2021-03-15 21:01:53 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PDCAサイクルを回そう！</h1>
+    <form action="pdca.php" method="post">
+        <p>計画(Plan)<br><textarea name="p" class="plan" cols="50" rows="20"></textarea></p>
+        <p>行動(Do)<br><textarea name="d" class="do" cols="50" rows="20"></textarea></p>
+        <p>評価(Check)<br><textarea name="c" class="check" cols="50" rows="20"></textarea></p>
+        <p>改善(Action)<br><textarea name="a" class="act" cols="50" rows="20"></textarea></p>
+        <p><input type="submit" value="登録"></p>
+    </form>
 
-    include('./junken.html');
-
-?>
+</body>
+</html>
---
+++ pdca.php
@@ -0,0 +1,25 @@
+<?php
+
+$p = $_POST["p"];
+$d = $_POST["d"];
+$c = $_POST["c"];
+$a = $_POST["a"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>あなたのPDCAサイクルは回った？</h1>
+        <p><?=$p?></p>
+        <p><?=$d?></p>
+        <p><?=$c?></p>
+        <p><?=$a?></p>
+</body>
+</html>
