9bf1963e17f846f20439015dba32202ca81fbd66
tomockki <80142152+tomockki@users.noreply.github.com>
2021-03-15 19:55:12 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,40 +1,5 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
-</head>
-<body>
-
-    
-
-
-</body>
-</html>
-
 <?php
-    echo '<h1 class="h1">ジャンケンゲーム</h1>
 
-    <div class="syobu">
-        <div class="te">あなたの手</div>
-        <div class="kekka">勝負！</div>
-    </div>
-
-    <div class="kekkatext"></div>
-
-    <h2 class="h2">↓↓↓選んでね！↓↓↓</h2>
-    
-    <div class="janken">
-        <button class="gu" ><img src="img/janken_gu.png" alt="" height="200" width="200"></button>
-        <button class="choki"><img src="img/janken_choki.png" alt="" height="200" width="200"></button>
-        <button class="pa"><img src="img/janken_pa.png" alt="" height="200" width="200"></button>
-    </div>
-    
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="js/app.js"></script>';
+    include('./junken.html');
 
 ?>
